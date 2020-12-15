5f80ab206ec0079ef8afc1ebb7074ef043fec63c
ntnghia1908 <66683614+ntnghia1908@users.noreply.github.com>
2020-12-15 20:01:28 +0700

    init

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.layout');
 
 @section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <p>Create new Pizza</p>
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="pizzas" method="POST">
+        <label for="name">Your name</label>
+        <input type="text" id="name" name="name">
+        <label for="type">Choose pizza type:</label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
+
+        <label for="base">Choose base type</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="gralic crust">Gralic Crust</option>
+            <option value="thin &*crispy">Thin & crispy</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
+
+
 </div>
 @endsection
